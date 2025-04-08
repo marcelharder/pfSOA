ff9cf46ce713213296fbe1e16431562f01f47f37
Marcel harder <marcelharder@protonmail.com>
2025-04-08 11:39:36 +0200

    publicIdChanged to string

--- Migrations/20250408093211_publiIdNaarString.Designer.cs
+++ Migrations/20250408093211_publiIdNaarString.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using pfSOA.data;
 
@@ -10,9 +11,11 @@ using pfSOA.data;
 namespace pfSOA.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250408093211_publiIdNaarString")]
+    partial class publiIdNaarString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- data/entities/Class_Photo.cs
+++ data/entities/Class_Photo.cs
@@ -4,7 +4,7 @@ public class Class_Photo
 {
     public int Id { get; set; }
     public int ProcedureId {get; set; }
-    public int PublicId { get; set; }
+    public string PublicId { get; set; } = "";
     public string Url {get; set;} = "";
     public string Description {get; set;} = "";
     public DateTime DateAdded {get; set;}
