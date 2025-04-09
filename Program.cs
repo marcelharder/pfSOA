fa0fa2ff51ba5a518f3cad0ca545845836c27846
Marcel harder <marcelharder@protonmail.com>
2025-04-09 12:19:32 +0200

    jsonoptions changed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApplicationServices(builder.Configuration);
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(Options => {Options.JsonSerializerOptions.PropertyNamingPolicy = null;});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- data/dtos/PhotoDto.cs
+++ data/dtos/PhotoDto.cs
@@ -3,7 +3,7 @@ namespace data.dtos;
 public class PhotoDto
 {
     public int Id { get; set; }
-     public int ProcedureId {get; set; }
+    public int ProcedureId {get; set; }
     public string PublicId { get; set; }="";
     public string Url {get; set;} = "";
     public string Description {get; set;} = "";
